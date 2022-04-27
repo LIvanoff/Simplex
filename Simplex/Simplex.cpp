3a0ef412241374b36a15204f4ba7d4b971b32a30
Leonid <drfrily@gmail.com>
2022-04-27 19:59:45 +0300

    Update

--- Simplex/Simplex.cpp
+++ Simplex/Simplex.cpp
@@ -7,8 +7,8 @@ using namespace std;
 
 class Simplex {
 private:
-	vector<int> Cj = { 3,5 }; // коэф зависимых переменных
-	vector<int> Cb = { 0,0,0,0 }; // коэф базисных пременных
+	vector<float> Cj = { 3,5 }; // коэф зависимых переменных
+	vector<float> Cb = { 0,0,0,0 }; // коэф базисных пременных
 	float delta1 = 0;
 	float delta2 = 0;
 	float Q = 0;
@@ -25,9 +25,20 @@ public:
 		}
 
 		// расчёт delta1, delta2 и Q
-		for(int i = 0; i < Cb.size(); i++)
+		for(int j = 0; j < 2; j++)
 		{
-
+			for(int i = 0; i < Cb.size(); i++)
+			{
+				if(j ==0)
+				{
+					delta1 += simp_arr[i][j] * Cb[i];
+				}
+				else
+				{
+					delta2 += simp_arr[i][j] * Cb[i];
+				}
+				Q += simp_arr[i][j] * Cb[i];
+			}
 		}
 		this->simp_arr[4][0] = delta1 - 3;
 		this->simp_arr[4][1] = delta2 - 5;
@@ -49,9 +60,9 @@ public:
 	{
 		bool flag = true;
 
-		for (int j = 1; j < 3; j++)
+		for (int j = 0; j < 3; j++)
 		{
-			if (this->simp_arr[4, j] < 0)
+			if (this->simp_arr[4][j] < 0)
 			{
 				flag = false;
 				break;
@@ -61,15 +72,48 @@ public:
 		return flag;
 	}
 
+	void copyArray(float (&copy_arr)[5][3])
+	{
+		for (int i = 0; i < 5; i++)
+		{
+			for (int j = 0; j < 3; j++)
+			{
+				copy_arr[i][j] = this-> simp_arr[i][j];
+			}
+		}
+	}
 	void Calculate()
 	{
 		while(!IsItEnd())
 		{
+			int col = findCol();
+			int row = findRow(col);
+			float ars = simp_arr[row][col];
+			float copy_arr[5][3];
+			copyArray(copy_arr);
 			for (int i = 0; i < 5; i++)
 			{
 				for (int j = 0; j < 3; j++)
 				{
-
+					if(j == col || j == row )
+					{
+						if(i == col && j == row)
+						{
+							simp_arr[i][j] = 1 /simp_arr[i][j];
+						}
+						else if( i == row )
+						{
+							simp_arr[i][j] = simp_arr[i][j] / ars;
+						}
+						else
+						{
+							simp_arr[i][j] = 1 / -simp_arr[i][j];
+						}
+					}
+					else
+					{
+						simp_arr[i][j] = ((copy_arr[i][j] * ars)-( * )) / ars;
+					}
 				}
 			}
 		}
@@ -122,5 +166,7 @@ int main()
 {
 	float arr[5][3] = { {10,70,570},{20,50,420},{300,400,5000},{200,100,3400}, {0,0,0} };
 	Simplex sp(arr);
+	sp.printSimplex();
+	sp.Calculate();
 	sp.printSimplex();
 }
